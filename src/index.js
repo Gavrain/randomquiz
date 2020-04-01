e0794cdec8e7eb396743ecbeb1c6d95aadf253ea
Gavrain <393033152@qq.com>
2020-04-01 16:54:40 +0800

    支持任意长度算式

--- src/index.js
+++ src/index.js
@@ -5,46 +5,59 @@
  * @LastEditTime : 2020-02-08 16:49:23
  * @no item name: \randomquiz\index.js
  */
-let XLSX = require('xlsx')
 let fs = require('fs')
-let path = require('path')
 
-// 同步读取
 let config = JSON.parse(fs.readFileSync('./config.json').toString())
-let limit = config.limit
-let row = config.row
-let col = config.col
-let rand = Math.random
-let round = Math.round
-let utils = XLSX.utils
+// 同步读取
 
-function gRow() {
-	let arr = new Array()
-	let i = 0
-	while (i < col) {
-		sign = rand() > 0.5 ? '+' : '-'
-		n1 = round(rand() * limit)
-		n2 = round(rand() * (sign == '+' ? limit - n1 : n1))
-		arr.push(`${n1}${sign}${n2}=`)
-		i++
-	}
-	return arr
+function getRandomQuiz(config) {
+  let XLSX = require('xlsx')
+  let utils = XLSX.utils
+  let path = require('path')
+
+  let { row, col } = config
+
+  function gFormula({ lower = 0, upper, numberCount }) {
+    let { random } = require('lodash')
+    let formula = []
+
+    for (let i = 0; i < numberCount; i++) {
+      let num = random(lower, upper)
+      lower -= num
+      upper -= num
+      if (i == 0) {
+        formula.push(num)
+        continue
+      }
+      formula.push(num < 0 ? num : `+${num}`)
+    }
+    return formula.join('')
+  }
+
+  function gMatrix({ col, row, upper, numberCount }) {
+    let matrix = []
+    for (let i = 0; i < row; i++) {
+      let arr = []
+      for (let j = 0; j < col; j++) {
+        arr.push(gFormula({ upper, numberCount }))
+      }
+      matrix.push(arr)
+    }
+    return matrix
+  }
+
+  let matrix = gMatrix(config)
+
+  try {
+    var workbook = XLSX.readFile(path.join(__dirname, 'quiz.xlsx'))
+  } catch (error) {
+    workbook = utils.book_new()
+  }
+  /* DO SOMETHING WITH workbook HERE */
+  utils.book_append_sheet(workbook, utils.aoa_to_sheet(matrix))
+  XLSX.writeFile(workbook, path.join(__dirname, 'quiz.xlsx'))
+  console.log(workbook.Sheets)
+  /* at this point, out.xlsb is a file that you can distribute */
 }
 
-let matrix = new Array()
-let i = 0
-while (i < row) {
-	matrix.push(gRow())
-	i++
-}
-
-try {
-	var workbook = XLSX.readFile(path.join(__dirname, 'quiz.xlsx'))
-} catch (error) {
-	workbook = utils.book_new()
-}
-/* DO SOMETHING WITH workbook HERE */
-utils.book_append_sheet(workbook, utils.aoa_to_sheet(matrix))
-XLSX.writeFile(workbook, path.join(__dirname, 'quiz.xlsx'))
-console.log(workbook.Sheets)
-/* at this point, out.xlsb is a file that you can distribute */
+getRandomQuiz(config)
